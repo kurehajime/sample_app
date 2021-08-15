40ed2657434e7a8fa3b03dd72662917fe8db6c6a
kurehajime <xiidec@gmail.com>
2021-08-15 08:55:29 +0000

    user destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
-  # get /users/new
-  def new
-    @user  = User.new
+  # get /users
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   # get /users/:id 
   def show
     @user = User.find(params[:id])
+  end
+  
+  # get /users/new
+  def new
+    @user  = User.new
   end
   
   # post /users + params
@@ -24,6 +30,11 @@ class UsersController < ApplicationController
     end
   end
   
+  # get /users/:id/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -34,10 +45,11 @@ class UsersController < ApplicationController
     end
   end
   
-  
-  # get /users/:id/edit
-  def edit
-    @user = User.find(params[:id])
+  # delete /users/:id
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
   
   private
@@ -59,4 +71,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+  
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+  
 end

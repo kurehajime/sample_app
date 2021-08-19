4591e4d32b66f7d51c86cbe7e084157b09a6969a
kurehajime <xiidec@gmail.com>
2021-08-19 12:11:31 +0000

    activate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = self.email.downcase }
-  
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
   validates :name ,
     presence:true,
     length:{maximum: 50}
@@ -42,5 +43,16 @@ class User < ApplicationRecord
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
+  private
+  
+    def downcase_email
+      self.email = email.downcase
+    end
+    
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
   
 end

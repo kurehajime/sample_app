0a07eef5cf87bf828c7416a3fa997ba78d165560
kurehajime <xiidec@gmail.com>
2021-08-21 14:18:12 +0000

    refactaring authenticated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,12 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  host = '95041134ef0f4ec7bdff89216cbcb42d.vfs.cloud9.ap-northeast-1.amazonaws.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # localhostで開発している場合は以下をお使いください
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
